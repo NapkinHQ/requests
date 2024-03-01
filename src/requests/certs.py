14989a8e609353b8ac9c90521c9047aef50a6230
klaviyo-nick <nick.sypteras@klaviyo.com>
2024-03-01 14:02:17 -0500

    package certifi ca bundle

--- src/requests/certs.py
+++ src/requests/certs.py
@@ -11,7 +11,15 @@ If you are packaging Requests, e.g., for a Linux distribution or a managed
 environment, you can change the definition of where() to return a separately
 packaged CA bundle.
 """
-from certifi import where
+
+import pathlib
+import os
+
+
+def where():
+    curr_dir = pathlib.Path(__file__).parent.resolve()
+    return os.path.join(curr_dir, "cacert.pem")
+
 
 if __name__ == "__main__":
     print(where())
